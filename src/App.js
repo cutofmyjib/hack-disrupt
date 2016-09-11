4f201c475a004991f6a4bdaa18e2b0c6b1fd382e
cutofmyjib <thedianalozano@gmail.com>
2016-09-10 23:29:48 -0500

    initial landing layout

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h3>What is your Twitter Handle?</h3>
-        <input type="text" />
-        <button>Submit</button>
+        <div className="background overlay"></div>
+        <header>
+          <h1 className="app-name">DESTINATE</h1>
+        </header>
+        <main>
+          <div className="twitter-form">
+            <h4 className="main-twitter">WHAT IS YOUR TWITTER HANDLE?</h4>
+            <input type="text" />
+            <button>Submit</button>
+          </div>
+        </main>
       </div>
     );
   }
