ff820b0b4b73a5d0d89fd5fac570f4ee6f7a8eb9
cutofmyjib <thedianalozano@gmail.com>
2016-09-11 09:14:50 -0700

    city layout

--- src/City.js
+++ src/City.js
@@ -12,8 +12,22 @@ class City extends Component {
 
   render() {
     return (
-      <div>
-        <h3>City: {this.state.city.info.city.name}</h3>
+      <div className="App">
+      <div className="secondary-header background overlay"></div>
+      <header className="header-nav">
+          <h1 className="app-name">
+            DESTIN<br />
+            <span className="pull"><i className="material-icons reverse">play_arrow</i></span>
+            ATE
+            </h1>
+        </header>
+        <main className="main-city">
+          <div className="city-name-div">
+            <img src={"http://nomadlist.com"+this.state.city.media.image['1000']} />
+            <h3 className="city-name">City: {this.state.city.info.city.name}</h3>
+          </div>
+          <div className="city-stats"></div>
+        </main>
       </div>
     );
   }
